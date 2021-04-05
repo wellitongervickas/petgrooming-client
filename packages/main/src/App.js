99df07cdbd18d13ac76b8da601f197e083c639d9
Welliton gervickas <wellitogervickas@gmail.com>
2021-04-04 23:52:54 -0300

    remove merlin file

--- packages/main/src/App.js
+++ packages/main/src/App.js
@@ -4,9 +4,8 @@ const CustomersList = React.lazy(() => import("customers/List"));
 
 const App = () => (
   <div>
-    <h1>Basic Host-Remote</h1>
     <h2>Main</h2>
-    <React.Suspense fallback="Loading list">
+    <React.Suspense fallback="Loading list of customers">
       <CustomersList />
     </React.Suspense>
   </div>
